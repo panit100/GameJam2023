32d719256965ade5672bd7755d76e350f7cd3b2f
Perth <61342646+Delayman@users.noreply.github.com>
2023-02-05 10:52:12 +0700

    Remove unnessary code

--- Assets/Script/Ability/ScaleSetting.cs
+++ Assets/Script/Ability/ScaleSetting.cs
@@ -7,15 +7,6 @@ namespace GameJam.GameData
 {
     public class ScaleSetting : MonoBehaviour
     {
-        [Tooltip("Set multiply value of scale UP")]
-        public float scaleUpMultiply;
-    
-        [Tooltip("Set multiply value of scale DOWN")]
-        public float scaleDownMultiply;
-        
-        [Tooltip("Set Normal Scale")]
-        public float normalScale;
-
         public float[] scaleValue;
         public int currentScale = 1;
         
