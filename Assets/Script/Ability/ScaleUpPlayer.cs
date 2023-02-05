a3fd549fea537765aa1cf82cf8afd84f524bd8d1
Estt <jojoeza10@gmail.com>
2023-02-05 12:17:03 +0700

    update soundpath_ScalePlayer

--- Assets/Script/Ability/ScaleUpPlayer.cs
+++ Assets/Script/Ability/ScaleUpPlayer.cs
@@ -28,8 +28,8 @@ public class ScaleUpPlayer : MonoBehaviour
 
         var _multiply = _scaleSet.scaleValue[_scaleSet.currentScale];
         _col.gameObject.transform.localScale = new Vector3(_multiply,_multiply,_multiply);
-        
-        if(soundManager != null)
+
+        if (soundManager != null)
             soundManager.PlayShrinkSFX();
 
 
